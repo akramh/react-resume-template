2992ae8b799e455f5aef5086ebf4c46217766e69
akramh <akramh@gmail.com>
2021-03-18 21:23:14 -0700

    adding new files

---
+++ src/Components/Workinprogress.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Workinprogress extends Component {
+  render() {
+
+
+    return (
+      <section id="workinprogress">
+
+         <div className="row section-head">
+
+            <div className="two columns header-col">
+
+               <h1><span>Work In Progress stay tuned...</span></h1>
+
+            </div>
+
+         </div>
+         </section>
+    )}
+   }
+
+export default Workinprogress;
