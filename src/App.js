3f9b259e33768d3741e9fc80ed8d1aff87f47d4d
akramh <akramh@gmail.com>
2021-03-19 20:51:47 -0700

    finishing it up

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        <Workinprogress  data={this.state.resumeData.main} />
+        <Footer  data={this.state.resumeData.main} />
       </div>
     );
   }
