124e42eab0fd3a00aa054e29a67612971164d3b8
akramh <akramh@gmail.com>
2021-03-23 07:36:31 -0700

    removing download button

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,17 +20,6 @@ class About extends Component {
             <h2>About Me</h2>
 
             <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-
-               </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
-            </div>
          </div>
       </div>
 
